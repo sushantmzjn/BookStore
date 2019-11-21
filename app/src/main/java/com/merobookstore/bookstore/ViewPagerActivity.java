2cdd41f0d41705531592017c5e8e7d38995e70e6
sushantmzjn <[sushantmzjn@gmail.com]>
2019-11-21 12:03:50 +0545

    initial commit

---
+++ app/src/main/java/com/merobookstore/bookstore/ViewPagerActivity.java
@@ -0,0 +1,26 @@
+package com.merobookstore.bookstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class ViewPagerActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager);
+
+        tabLayout = findViewById(R.id.tabId);
+        viewPager = findViewById(R.id.viewpager);
+
+
+    }
+}
